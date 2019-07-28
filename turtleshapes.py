1de906c8392bc9164f4f043d5d7db0421510774f
nardeen21-meet <nardeen21@meet.mit.edu>
2019-07-28 11:58:23 +0300

    Stopping here

---
+++ turtleshapes.py
@@ -0,0 +1,13 @@
+import turtle
+num = 5
+for i in range(num_pts):
+    turtle.left(360/num_pts)
+    turtle.forward(100)
+    turtle.mainloop()
+    result = []
+    for count in range(1,n):
+    if count % 3 == 0:
+        result.append('fizz")
+                     else:
+                      result.append(count)
+
